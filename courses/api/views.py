fea678461e69e34ff6a82b97e32201c004740d6f
Femi-ID <104504319+Femi-ID@users.noreply.github.com>
2024-01-12 00:57:18 +0100

    Added view to display course contents for only enrolled users.

--- courses/api/views.py
+++ courses/api/views.py
@@ -9,6 +9,8 @@ from rest_framework.authentication import BasicAuthentication
 from rest_framework.permissions import IsAuthenticated
 from rest_framework import viewsets
 from rest_framework.decorators import action
+from .permissions import IsEnrolled
+from .serializers import CourseWithContentsSerializer
 
 
 class SubjectListView(generics.ListAPIView):
@@ -38,9 +40,13 @@ class CourseViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = Course.objects.all()
     serializer_class = CourseSerializer
 
-    @action(detail=True, methods=['post'], authentication_classes=[BasicAuthentication],
-            permission_classes=[IsAuthenticated])
-    def enroll(self, request, *args, **kwargs):
-        course = self.get_object()
-        course.student.add(request.user)
-        return Response({'enrolled': True})
+    @action(detail=True, methods=['get', 'post'], serializer_class=CourseWithContentsSerializer,
+            authentication_classes=[BasicAuthentication],
+            permission_classes=[IsAuthenticated, IsEnrolled])
+    def contents(self, request, *args, **kwargs):
+        return self.retrieve(request, *args, **kwargs)
+
+    # def enroll(self, request, *args, **kwargs):
+    #     course = self.get_object()
+    #     course.student.add(request.user)
+    #     return Response({'enrolled': True})
